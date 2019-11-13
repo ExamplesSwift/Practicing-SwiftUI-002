dabddbf883ea061841ab757567e3a59c6920c0e9
Henry Ambicho T <yiperu@gmail.com>
2019-11-13 00:35:25 -0300

    Add .multilineTextAlignment(.center) propetty fo see how aling in Text

--- P01E04-Starter/ContentView.swift
+++ P01E04-Starter/ContentView.swift
@@ -33,6 +33,7 @@ struct ContentView: View {
     Text("Welcome to the Ray Wenderlich course on SwiftUI. In this course we are going to have an EPIC time working with all that SwiftUI has to offer. Remember, the answers are in the documentation :]")
     .lineLimit(2)
       .minimumScaleFactor(0.5)
+      .multilineTextAlignment(.center)
       .truncationMode(.middle)
       .foregroundColor(Color.red)
       .padding()
