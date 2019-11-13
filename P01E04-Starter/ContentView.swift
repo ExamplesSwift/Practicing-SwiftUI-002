75a74e4a72b41aab231090ce5ebea6b2bdd5ee0f
Henry Ambicho T <yiperu@gmail.com>
2019-11-13 00:31:51 -0300

    I can specified acount of lines

--- P01E04-Starter/ContentView.swift
+++ P01E04-Starter/ContentView.swift
@@ -31,6 +31,7 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     Text("Welcome to the Ray Wenderlich course on SwiftUI. In this course we are going to have an EPIC time working with all that SwiftUI has to offer. Remember, the answers are in the documentation :]")
+    .lineLimit(1)
       .foregroundColor(Color.red)
       .padding()
       .background(Color.gray)
